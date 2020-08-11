b418ddf6bc80d3920411475c18c33705c488e03b
JuliaVal821 <julia.kolgatina@gmail.com>
2020-08-11 17:15:06 -0400

    Add Code Wars Solution 'Hello World'

---
+++ HelloWorld.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+
+function greet (str) {
+    var str = "hello world!";
+    console.log(str);
+    return "hello world!";
+}
